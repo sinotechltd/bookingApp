7b80d2b6789bdb7d8543016eea9fa14b07b0df29
Collins Kipruto <53892938+collinskandie@users.noreply.github.com>
2022-09-09 13:22:04 +0300

    working

---
+++ resources/views/deletemodal.blade.php
@@ -0,0 +1,19 @@
+<!-- Button trigger modal -->
+    <!--Edit  Modal -->
+    <div class="modal fade" id="deleteentry" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete record</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body"> 
+                    <h4>Are you sure you want to delete?</h4>                 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <button type="button" class="btn btn-primary">Yes</button>
+                </div>
+            </div>
+        </div>
+    </div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,114 +12,11 @@
 
 <body>
     @include('navbar')
+    @include('editmodal')
+    @include('deletemodal')
+    @include('bookform')
+    @include('approve')
 
-    <!-- pop up booking form Modal -->
-    <div class="modal modal-xl" id="addbtn" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <form>
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">OB PRODUCTION BOOKING FORM</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Program title</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>Programs</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                            <div id="emailHelp" class="form-text">Select your program</div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Topic</label>
-                            <input type="text" name="program_topic" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">Topic.</div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Producer/Director</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="0">first</option>
-                                <option value="1">1</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>                            <div id="emailHelp" class="form-text">Select your Producer.</div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Date</label>
-                            <input type="date" name="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Location</label>
-                            <input type="text" name="location" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Equipments</label> 
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="0">first</option>
-                                <option value="1">1</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Setting/Rigging time</label>
-                            <input type="time" name="setting_time" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Rehearsal Time</label>
-                            <input type="time" name="rehearsal_time" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Designer</label>
-                            <input type="text" name="designer" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Guests</label>
-                            <input type="text" name="guests" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Presenters</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="0">first</option>
-                                <option value="1">1</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Special Remarks</label>
-                            <input type="text" name="remarks" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Operation Crew</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="0">first</option>
-                                <option value="1">1</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Team/Shift Leader</label>
-                            <select class="form-select" aria-label="Default select example">
-                                <option value="0">first</option>
-                                <option value="1">1</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
     <div class="container">
         <div class="row">
             <div class="col">
@@ -160,6 +57,9 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @empty($bookings)
+                     <p>@error('fail'){{ $message}} @enderror</p>  
+                    @else{                   
                     @foreach($bookings as $booking)
                     <tr>
                         <th scope="row">{{ $booking['booking_id'] }}</th>
@@ -176,14 +76,14 @@
                         <th> {{ $booking['approval_level3'] }}</th>
                         <th>
                             <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                                <button type="button" class="btn btn-danger">Delete</button>
-
-                                <button type="button" class="btn btn-success">Edit</button>
+                                <button type="button" class="btn btn-danger mr-3" data-bs-toggle="modal" data-bs-target="#deleteentry">Delete</button>
+                                <button type="button" class="btn btn-primary mr-3" data-bs-toggle="modal" data-bs-target="#editentry">Edit</button>
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#approve">Approve</button>
                             </div>
                         </th>
                     </tr>
                     @endforeach
-
+                    @endempty
                 </tbody>
             </table>
         </div>
